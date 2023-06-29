90b6837b8ea6052b45eeffa7d002ef81aa7725c2
jugiros <31550758+jugiros@users.noreply.github.com>
2023-06-29 10:56:53 -0500

    feat: Integration of the component to display alerts.

---
+++ src/components/AlertComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="alert-style" v-if="props.viewData">
+    <p>{{ props.msg }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// eslint-disable-next-line no-undef
+const props = defineProps<{
+  msg: string;
+  viewData: boolean;
+}>();
+</script>
+
+<style scoped lang="scss">
+.alert-style {
+  background-color: #ff5757;
+  border-radius: 8px;
+  text-align: justify;
+  p {
+    color: #fff;
+    font-size: 12px;
+    margin: 5px;
+  }
+}
+</style>
