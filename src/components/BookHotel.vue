fdda88d5cf5cdd2d4397fb5f0c295c6b539e0ee0
jugiros <31550758+jugiros@users.noreply.github.com>
2023-06-29 19:33:17 -0500

    feat: Final integration of the functionality to obtain the best hotel for reservations. Update to display the best hotel for reservations.

--- src/components/BookHotel.vue
+++ src/components/BookHotel.vue
@@ -1,47 +1,46 @@
 <template>
   <div class="container-book-hotel">
-    <h3>Valida las fechas para tu reserva</h3>
+    <h3>Valida las fechas y tipo de cliente para tu reserva</h3>
     <div class="book-hotel">
-      <button class="btn-date">
-        <label for="btn-modal"> Agregar fechas </label>
+      <button>
+        <label for="btn-modal"> Agregar información </label>
       </button>
+      <button class="warning-button" @click="restartProcess">Limpiar datos</button>
       <input type="checkbox" id="btn-modal" />
       <div class="container-modal">
         <div class="content-modal">
-          <h3>Fechas para mi reserva</h3>
+          <h3>
+            Fechas para mi reserva
+            <label class="label-close" for="btn-modal">X</label>
+          </h3>
           <hr />
           <div>
-            <input type="date" />
-            <button class="main-button">Agregar</button>
+            <label class="label-client">Cliente: </label>
+            <select name="select" v-model="selectedType">
+              <option value="0" selected>Normal</option>
+              <option value="1">Recompensa</option>
+            </select>
           </div>
+          <div>
+            <input type="date" v-model="selectedDate" :min="currentDate" />
+            <button class="main-button" @click="addDate">Agregar</button>
+          </div>
+          <alert-component />
           <hr />
-          <ul>
-            <li>
-              20 de enero del 2022<button class="delete-button">
-                <img src="../assets/borrar.png" alt="x" />
-              </button>
-            </li>
-            <li>
-              20 de enero del 2022<button class="delete-button">
-                <img src="../assets/borrar.png" alt="x" />
-              </button>
-            </li>
-            <li>
-              20 de enero del 2022<button class="delete-button">
-                <img src="../assets/borrar.png" alt="x" />
-              </button>
-            </li>
-            <li>
-              20 de enero del 2022<button class="delete-button">
+          <ul v-if="arrDateView && lengthArr > 0">
+            <li v-for="(day, index) in arrDateView" :key="index">
+              {{ day.dateString }}
+              <button class="delete-button" @click="deleteDate(index)">
                 <img src="../assets/borrar.png" alt="x" />
               </button>
             </li>
           </ul>
-          <hr>
+          <h4 v-else>No se tiene agregadas fechas para la reserva.</h4>
+          <hr />
           <div>
-            <button>Buscar Hotel</button>
+            <button @click="searchHotel">Buscar Hotel</button>
             <button class="warning-button">
-              <label for="btn-modal">Cancelar</label>
+              <label @click="restartProcess">Reiniciar</label>
             </button>
           </div>
         </div>
@@ -51,4 +50,96 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import router from "@/router";
+import AlertComponent from "@/components/AlertComponent.vue";
+import { Day } from "@/interfaces/day";
+import { viewAlert, getBestOffer } from "@/helpers/functions";
+import { Hotel } from "@/interfaces/hotel";
+import { useHotelStore } from "@/stores/hotel";
+
+let currentDate = ref<string>("");
+let selectedDate = ref<string>("");
+let arrDateView = ref<Day[]>([]);
+let lengthArr = ref<number>(0);
+let selectedType = ref<string>("0");
+
+let dateAdd: Date;
+let numberDay = 0;
+
+let dayView: Day;
+
+const hotelStore = useHotelStore();
+currentDate.value = new Date().toISOString().split("T")[0];
+selectedDate.value = new Date().toISOString().split("T")[0];
+
+const addDate = () => {
+  let element: Day[] = arrDateView.value.filter((day) => {
+    return day.dateString == selectedDate.value;
+  });
+
+  if (element.length > 0) {
+    viewAlert("La fecha seleccionada ya está agregada", "info");
+    return;
+  }
+
+  dateAdd = new Date(selectedDate.value);
+  dateAdd.setDate(dateAdd.getDate() + 1);
+  numberDay = dateAdd.getDay();
+
+  let valBoolean = numberDay == 0 || numberDay == 6 ? true : false;
+
+  dayView = {
+    weekend: valBoolean,
+    dateString: selectedDate.value,
+  };
+
+  arrDateView.value?.push(dayView);
+  lengthArr.value = arrDateView.value ? arrDateView.value?.length : 0;
+
+  viewAlert("Fecha agregada de manera correcta", "success");
+};
+
+const deleteDate = (index: number) => {
+  arrDateView.value?.splice(index, 1);
+  lengthArr.value = arrDateView.value ? arrDateView.value?.length : 0;
+  viewAlert("Fecha eliminada de manera correcta", "warning");
+};
+
+const searchHotel = () => {
+  if (arrDateView && arrDateView.value.length > 0) {
+    const hotel: Hotel = getBestOffer(
+      arrDateView.value,
+      selectedType.value == "0" ? false : true
+    );
+    console.log(hotel);
+    hotelStore.setViewHotel(true);
+    hotelStore.setBestHotel(hotel);
+    router.push({name: "home"});
+  } else {
+    viewAlert("Debes agregar una fecha para continuar.", "error");
+  }
+};
+
+const restartProcess = () => {
+  arrDateView.value = [];
+  selectedDate.value = currentDate.value;
+  selectedType.value = "0";
+  lengthArr.value = 0;
+  hotelStore.setViewHotel(false);
+};
+</script>
+
+<style lang="scss" scoped>
+.container-book-hotel {
+  .label-client {
+    color: #555555;
+    font-weight: bold;
+  }
+  .label-close {
+    margin-left: 100px;
+    color: #e22e2e;
+  }
+}
+</style>
--- src/components/HotelCard.vue
+++ src/components/HotelCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="gallery">
+  <div class="gallery" v-if="!viewBestHotel">
     <div
       class="card"
       v-for="(hotel, index) in hotelStore.arrHotels"
@@ -9,22 +9,56 @@
       <div class="container-app">
         <h3>
           <b>{{ hotel.name }}</b>
+          <router-link :to="'/' + hotel.nameVista">
+            <img class="img-view-page" src="../assets/eye.png">
+          </router-link>
         </h3>
         <p>{{ hotel.description }}</p>
-        <qualification-stars :qualification="hotel.qualification"/>
+        <qualification-stars :qualification="hotel.qualification" />
         <h4><b>Costo cliente normal</b></h4>
-        <p>Entre semana: {{hotel.regularCost}} - Fin de semana: {{hotel.regularCostWeekend}}</p>
+        <p>
+          Entre semana: {{ hotel.regularCost }} - Fin de semana:
+          {{ hotel.regularCostWeekend }}
+        </p>
         <h4><b>Costo cliente recompensa</b></h4>
-        <p>Entre semana: {{hotel.rewardCost}} - Fin de semana: {{hotel.rewardCostWeekend}}</p>
+        <p>
+          Entre semana: {{ hotel.rewardCost }} - Fin de semana:
+          {{ hotel.rewardCostWeekend }}
+        </p>
       </div>
     </div>
+  </div>
+  <div v-else>
+    <h1>Nuestra mejor opción para ti</h1>
+    <view-hotel :view-hotel="bestHotel"/>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useHotelStore } from "@/stores/hotel";
+import { storeToRefs } from "pinia";
+
 import QualificationStars from "@/components/QualificationStars.vue";
+import ViewHotel from "@/components/viewHotel.vue";
+
 const hotelStore = useHotelStore();
+let { bestHotel, viewBestHotel } = storeToRefs(hotelStore);
+console.log(bestHotel);
 </script>
 
-<style />
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+}
+.img-view-page {
+  height: 20px;
+  width: 20px;
+  margin-left: 20px;
+}
+.gallery {
+  display: grid;
+  gap: 1rem;
+  grid-auto-rows: 33rem;
+  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+}
+</style>
